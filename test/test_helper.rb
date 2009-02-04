763a71fe91248a77a30497b30b4ac970ab2cf309
Tore Darell <toredarell@gmail.com>
2009-02-04 23:14:02 +0100

    Remove unnecessary loading of Rails env

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,4 @@
-ENV['RAILS_ENV'] = 'test'
-ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
-
+require 'rubygems'
+require 'active_support'
 require 'test/unit'
-require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb')) 
+require File.join(File.dirname(__FILE__), '..', 'lib', 'lucy')
